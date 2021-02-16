e350658c76703c21c4026259541ae270e7e38354
JannisN <none@none.none>
2021-02-16 19:22:58 +0100

    m

--- generate.py
+++ generate.py
@@ -1,5 +1,12 @@
 vulkanLib = input("Vulkan Lib:")
 glfwLib = input("Glfw Lib:")
+openclLib = input("OpenCL Lib:")
+cLib = input("C Lib directory:")
+
+#C://Users/Admin/Desktop/dlang/dubtest/lib/vulkan-1.lib
+#C://Users/Admin/Desktop/dlang/dubtest/lib/glfw.lib
+#C://Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/lib/x64/OpenCL.lib
+#C://Program Files (x86)/Windows Kits/10/Lib/10.0.17763.0/um/x64
 
 import codecs
 with codecs.open("dub.sdl", "x", encoding='utf8') as f:
@@ -13,5 +20,19 @@ with codecs.open("dub.sdl", "x", encoding='utf8') as f:
 	f.write('buildRequirements "allowWarnings"\n')
 	f.write('stringImportPaths "views"\n')
 	f.write('\n')
-	f.write('lflags "' + vulkanLib + '" "' + glfwLib + '" platform="windows"\n')
+	#f.write('lflags "' + vulkanLib + '" "' + glfwLib + '" "/NODEFAULTLIB:libvcruntime.lib" "/NODEFAULTLIB:libcmt.lib" platform="windows"\n')
+	f.write('lflags ')
+	#f.write('"' + cLib + '/msvcrt.lib" ')
+	f.write('"' + cLib + '/opengl32.lib" ')
+	#f.write('"' + cLib + '/kernel32.lib" ')
+	f.write('"' + cLib + '/user32.lib" ')
+	f.write('"' + cLib + '/gdi32.lib" ')
+	#f.write('"' + cLib + '/winspool.lib" ')
+	#f.write('"' + cLib + '/shell32.lib" ')
+	#f.write('"' + cLib + '/ole32.lib" ')
+	#f.write('"' + cLib + '/oleaut32.lib" ')
+	#f.write('"' + cLib + '/uuid.lib" ')
+	#f.write('"' + cLib + '/comdlg32.lib" ')
+	#f.write('"' + cLib + '/advapi32.lib" ')
+	f.write('"' + vulkanLib + '" "' + glfwLib + '" "' + openclLib + '" "/NODEFAULTLIB:libvcruntime.lib" "/NODEFAULTLIB:libcmt.lib" platform="windows"\n')
 	f.close()
