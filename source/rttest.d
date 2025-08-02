647d1937bc54db06184a4065aaf74dddd7f90baf
Jannis <jannispj@gmail.com>
2025-08-02 17:33:58 +0200

    m

--- source/rttest.d
+++ source/rttest.d
@@ -39,7 +39,7 @@ struct TestApp(ECS) {
 		rot[1] = 0.0;
 
 		enum string cubeCode = import("cube.wobj");
-		enum string sphereCode = import("sphereflat.wobj");
+		enum string sphereCode = import("sphere.wobj");
 		cubeModel = models.add().add!WavefrontModel(cubeCode).entityId;
 		sphereModel = models.add().add!WavefrontModel(sphereCode).add!ProceduralModel(0, array(-1.0f, -1.0f, -1.0f), array(1.0f, 1.0f, 1.0f)).entityId;
 
@@ -92,7 +92,8 @@ struct TestApp(ECS) {
 			drawable.dpos = Tensor!(float, 3)(0, 0, 0);
 			drawable.scale = Tensor!(float, 3)(1, 1, 1);
 			drawable.rot = Tensor!(float, 3)(0, 0, 0);
-			drawable.rgb = Tensor!(float, 3)(0.0, 1.0, 1.0);
+			drawable.rgb = Tensor!(float, 3)(0.0, 0.0, 0.0);
+			//drawable.rgb = Tensor!(float, 3)(0.0, 1.0, 1.0);
 			drawable.modelId = cast(uint)sphereModel;
 			objects.add().add!Drawable(drawable);
 		}
